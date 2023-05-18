56ec0bfe862050b65935d780772c7a4dce61dfa5
Matt Rodrigues <ec.mattrodrigues@gmail.com>
2023-05-18 20:27:27 -0300

    Created Ranking Route and Update Dump.sql

--- src/routes/getRoutes.js
+++ src/routes/getRoutes.js
@@ -11,12 +11,15 @@ import { getUrlById } from "../controllers/getUrlByIdController.js";
 // Imports of openUrl
 import { openShortUrl } from "../controllers/openShortUrlController.js";
 
-// import get Me
+// import of get Me
 import { getMe } from "../controllers/getMeController.js";
+
+// import of ranking
+import { ranking } from "../controllers/rankingController.js";
 
 Get.get("/urls/:id", getUrlById);
 Get.get("/urls/open/:shortUrl", openShortUrl);
 Get.get("/users/me", getMe);
-Get.get("/ranking");
+Get.get("/ranking",ranking);
 
 export default Get;
