93f684f047f616100eaa98d1e7abd73d293cb61e
Matt Rodrigues <ec.mattrodrigues@gmail.com>
2023-05-23 12:09:24 -0300

    Fix messages of error for users

--- src/middlewares/signUpMiddleware.js
+++ src/middlewares/signUpMiddleware.js
@@ -6,19 +6,16 @@ export default function signUpValidation(req, res, next){
     
     if(signUpSchema.validate({Name: name, Email: email, Password: password}).error !== undefined){
 
-        if(signUpSchema.validate({Name: name, Email: email, Password: password}).error.message === '"Name" is not allowed to be empty'){
-            return res.status(422).send("O campo NOME é obrigatório.")
-        }
         if(signUpSchema.validate({Name: name, Email: email, Password: password}).error.message === '"Name" length must be at least 3 characters long'){
             return res.status(422).send("O campo NOME deve conter mais de 3 caracteres.")
         }
-        if(signUpSchema.validate({Name: name, Email: email, Password: password}).error.message === '"Email" is not allowed to be empty'){
+        if(signUpSchema.validate({Name: name, Email: email, Password: password}).error.message === '"Email" is required'){
             return res.status(422).send("O campo EMAIL é obrigatório.")
         }
         if(signUpSchema.validate({Name: name, Email: email, Password: password}).error.message === '"Email" must be a valid email'){
             return res.status(422).send("O campo EMAIL deve conter um email válido.")
         }
-        if(signUpSchema.validate({Name: name, Email: email, Password: password}).error.message === '"Password" is not allowed to be empty'){
+        if(signUpSchema.validate({Name: name, Email: email, Password: password}).error.message === '"Password" is required'){
             return res.status(422).send("O campo SENHA é obrigatório.")
         }
         if(signUpSchema.validate({Name: name, Email: email, Password: password}).error.message === '"Password" length must be at least 3 characters long'){
